cb7dea2d9b55d685484e8a5c31bdbecdd7e2e0ee
Christoph Reiter <reiter.christoph@gmail.com>
2024-03-07 11:17:40 +0100

    Symfony 6 preparations

--- src/DependencyInjection/DbpRelayFrontendExtension.php
+++ src/DependencyInjection/DbpRelayFrontendExtension.php
@@ -14,7 +14,7 @@ class DbpRelayFrontendExtension extends ConfigurableExtension
 {
     use ExtensionTrait;
 
-    public function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    public function loadInternal(array $mergedConfig, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader(
             $container,
