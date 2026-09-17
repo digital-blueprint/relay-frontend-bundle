48bf1c953a5a422110f00eafc01cc7fce3847b5b
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-17 15:30:33 +0200

    Port away from the deprecated UserAuthTrait
    Use TestClient instead

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -5,39 +5,32 @@ declare(strict_types=1);
 namespace Dbp\Relay\FrontendBundle\Tests;
 
 use Dbp\Relay\CoreBundle\TestUtils\AbstractApiTest;
-use Dbp\Relay\CoreBundle\TestUtils\UserAuthTrait;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiTest extends AbstractApiTest
 {
-    use UserAuthTrait;
-
     public function testBasics()
     {
-        $client = self::createClient();
-        $response = $client->request('GET', '/frontend/users');
+        $response = $this->testClient->get('/frontend/users', token: null);
         $this->assertSame(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
 
-        $response = $client->request('GET', '/frontend/users/foobar');
+        $response = $this->testClient->get('/frontend/users/foobar', token: null);
         $this->assertSame(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
     }
 
     public function testPostPut()
     {
-        $client = self::createClient();
-        $response = $client->request('POST', '/frontend/users');
+        $response = $this->testClient->request('POST', '/frontend/users', token: null);
         $this->assertSame(Response::HTTP_METHOD_NOT_ALLOWED, $response->getStatusCode());
 
-        $response = $client->request('PUT', '/frontend/users');
+        $response = $this->testClient->request('PUT', '/frontend/users', token: null);
         $this->assertSame(Response::HTTP_METHOD_NOT_ALLOWED, $response->getStatusCode());
     }
 
     public function testAuthCollection()
     {
-        $client = $this->withUser('foobar', ['ROLE_XXX'], '42');
-        $response = $client->request('GET', '/frontend/users', ['headers' => [
-            'Authorization' => 'Bearer 42',
-        ]]);
+        $this->testClient->setUpUser('foobar', symfonyRoles: ['ROLE_XXX']);
+        $response = $this->testClient->get('/frontend/users');
         $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
         $content = $response->getContent();
         $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
@@ -50,10 +43,8 @@ class ApiTest extends AbstractApiTest
 
     public function testAuthUserWithoutRole()
     {
-        $client = $this->withUser('foobar', [], '42');
-        $response = $client->request('GET', '/frontend/users/foobar', ['headers' => [
-            'Authorization' => 'Bearer 42',
-        ]]);
+        $this->testClient->setUpUser('foobar');
+        $response = $this->testClient->get('/frontend/users/foobar');
         $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
         $content = $response->getContent();
         $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
@@ -63,10 +54,8 @@ class ApiTest extends AbstractApiTest
 
     public function testAuthUserWithSymfonyRole()
     {
-        $client = $this->withUser('foobar', ['ROLE_XXX'], '42');
-        $response = $client->request('GET', '/frontend/users/foobar', ['headers' => [
-            'Authorization' => 'Bearer 42',
-        ]]);
+        $this->testClient->setUpUser('foobar', symfonyRoles: ['ROLE_XXX']);
+        $response = $this->testClient->get('/frontend/users/foobar');
         $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
         $content = $response->getContent();
         $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
@@ -76,10 +65,8 @@ class ApiTest extends AbstractApiTest
 
     public function testAuthUserWithSymfonyRoleAndAddedRole()
     {
-        $client = $this->withUser('admin', ['ROLE_XXX'], '42');
-        $response = $client->request('GET', '/frontend/users/admin', ['headers' => [
-            'Authorization' => 'Bearer 42',
-        ]]);
+        $this->testClient->setUpUser('admin', symfonyRoles: ['ROLE_XXX']);
+        $response = $this->testClient->get('/frontend/users/admin');
         $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
         $content = $response->getContent();
         $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
@@ -91,10 +78,8 @@ class ApiTest extends AbstractApiTest
 
     public function testAuthUserWithAddedRole()
     {
-        $client = $this->withUser('franz', [], '42');
-        $response = $client->request('GET', '/frontend/users/franz', ['headers' => [
-            'Authorization' => 'Bearer 42',
-        ]]);
+        $this->testClient->setUpUser('franz');
+        $response = $this->testClient->get('/frontend/users/franz');
         $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
         $content = $response->getContent();
         $data = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
@@ -104,10 +89,8 @@ class ApiTest extends AbstractApiTest
 
     public function testAuthUserNotFound()
     {
-        $client = $this->withUser('foobar', ['ROLE_XXX'], '42');
-        $response = $client->request('GET', '/frontend/users/foobar2', ['headers' => [
-            'Authorization' => 'Bearer 42',
-        ]]);
+        $this->testClient->setUpUser('foobar', symfonyRoles: ['ROLE_XXX']);
+        $response = $this->testClient->get('/frontend/users/foobar2');
         $this->assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
     }
 }
